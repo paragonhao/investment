67008aa4a663b480eb780d545f3be90558228846
xiahao <wangxiahaosin@gmail.com>
2018-10-04 19:59:26 -0700

    add hw 2

---
+++ lecture2p.R
@@ -0,0 +1,36 @@
+# Problem Set 2 Solution Template
+# mysoln is a list to store your answers
+
+# the first element of the list is a vector of names for the students in your group
+# make sure these match the names shown on the pdf document provided by the MFE office
+# using group 1 as an example:
+mysoln = list(student = c("Molin Liang", "Meghana Rao", "Chengbo Du", "Shardul Kulkarni"))
+
+# 1
+
+# your intermediary code to get your answers here
+
+# save down your final answers for part a, b, and c
+a = c(price,ytm) #ytm in decimal form
+b = c(price,ytm) #ytm in decimal form
+c = c(price,ytm) #ytm in decimal form
+
+# add answers to list for "Q1"
+mysoln[["Q1"]] = list(a=a, b=b, c=c)
+
+# 2
+
+# your intermediary code to get your answers here
+
+# save down your final answers
+a = c(r.6month, r.1yr) #in decimal form
+
+# add answers to list for "Q2"
+mysoln[["Q2"]] = list(a=a)
+
+# 3
+# your intermediary code to get your answers here
+
+# save down your final answers
+
+# Put the answer in your PDF writeup
